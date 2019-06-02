730cb338586ef94f6052993d099f0624b6153e9e
estebanluciano <estebanluciano@hotmail.com>
2019-06-02 17:33:04 -0300

    Primeros ejercicios de PHP

---
+++ Ejercicios.php
@@ -0,0 +1,24 @@
+<H2>Ejercicio's</H2>
+<?php
+// comentario de linea
+/* comentarios 
+multilinea */
+
+// string
+$uno = "contenido de uno";
+
+// integer
+$dos = 234;
+
+echo "Uno: ".$uno."<br/>";
+echo "Dos: ".$dos."<hr/>";
+
+// cuadrados de los primeros 30 numeros enteros
+for ($i=1; $i <= 30; $i++) { 
+	echo $i." * ".$i." = ".($i*$i)."<br/>";
+}
+echo "<hr/>";
+
+//
+
+?>
